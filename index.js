357d1ac2221ff9d31b72c5a7f95eff26b33af806
khanh-ptit <168727980+khanh-ptit@users.noreply.github.com>
2024-07-18 08:56:01 +0700

    1.2 Xây dựng layout chung

--- index.js
+++ index.js
@@ -4,16 +4,24 @@ require("dotenv").config()
 const database = require("./config/database")
 database.connect()
 
-const route = require('./routes/client/index.route')
+const routeAdmin = require('./routes/admin/index.route')
+const routeClient = require('./routes/client/index.route')
+const systemConfig = require('./config/system')
+
 const app = express()
 const port = process.env.PORT
 
 app.set("views", "./views")
 app.set("view engine", "pug")
 
+// App local variables
+app.locals.prefixAdmin = systemConfig.prefixAdmin
+
+
 app.use(express.static("public"))
 
-route(app)
+routeClient(app)
+routeAdmin(app)
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
