69f5cb4eb3266b447ed3b5e33ee79f5358fcf138
khanh-ptit <168727980+khanh-ptit@users.noreply.github.com>
2024-07-19 22:21:35 +0700

    1.2 Tối ưu phần tìm kiếm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,7 @@
 const Product = require('../../models/product.model')
 
 const filterStatusHelper = require('../../helpers/filterStatus')
+const searchHelper = require('../../helpers/search')
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
     // console.log(req.query.status)
@@ -16,12 +17,19 @@ module.exports.index = async (req, res) => {
         find.status = req.query.status
     }
 
-    let keyword = ""
-    if (req.query.keyword) {
-        keyword = req.query.keyword
+    // Tìm kiếm
+    // let keyword = ""
+    // if (req.query.keyword) {
+    //     keyword = req.query.keyword
 
-        const regex = new RegExp(keyword, "i")
-        find.title = regex
+    //     const regex = new RegExp(keyword, "i")
+    //     find.title = regex
+    // }
+    const objectSearch = searchHelper(req.query)
+    console.log(objectSearch)
+    
+    if (objectSearch.regex) {
+        find.title = objectSearch.regex
     }
 
     const products = await Product.find(find)
@@ -31,6 +39,6 @@ module.exports.index = async (req, res) => {
         pageTitle: "Danh sách sản phẩm",
         products: products,
         filterStatus: filterStatus,
-        keyword: keyword
+        keyword: objectSearch.keyword
     })
 }
