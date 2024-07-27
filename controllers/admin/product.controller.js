0096cc4e78e59255ba36e79caf4dc5bbf4ceb6ba
khanh-ptit <168727980+khanh-ptit@users.noreply.github.com>
2024-07-27 23:13:56 +0700

    4.2 Xóa mềm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -104,6 +104,13 @@ module.exports.changeMulti = async (req, res) => {
 //[DELETE] /admin/products/delete/:id
 module.exports.deleteItem = async (req, res) => {
     const id = req.params.id
-    await Product.deleteOne({_id: id})
+    // await Product.deleteOne({_id: id}) Xóa cứng
+    await Product.updateOne({
+        _id: id
+    }, {
+        deleted: true,
+        deletedAt: new Date()
+    })
+
     res.redirect("back")
 }
