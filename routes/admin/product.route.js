bed18018ec3fb16500121fb99c917545bfd5ebc7
khanh-ptit <168727980+khanh-ptit@users.noreply.github.com>
2024-08-04 22:32:48 +0700

    Thêm danh mục sản phẩm (đang tối ưu phần tìm kiếm)

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -2,11 +2,6 @@ const express = require('express')
 const router = express.Router()
 
 const multer = require("multer")
-const storageMulter = require("../../helpers/storageMulter")
-// const upload = multer({
-//     storage: storageMulter()
-// })
-
 const upload = multer()
 
 const controller = require("../../controllers/admin/product.controller")
