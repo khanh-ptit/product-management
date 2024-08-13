2c54d4334bbba31ad0f9c160b8b9b368e44878df
khanh-ptit <168727980+khanh-ptit@users.noreply.github.com>
2024-08-13 23:43:37 +0700

    Làm trang sửa tài khoản

--- routes/admin/account.route.js
+++ routes/admin/account.route.js
@@ -12,6 +12,10 @@ router.get('/', controller.index)
 
 router.get('/create', controller.create)
 
+router.get('/edit/:id', controller.edit)
+
+router.patch('/edit/:id', upload.single('avatar'), uploadCloud.upload, validate.editPatch, controller.editPatch)
+
 router.post('/create', upload.single('avatar'), uploadCloud.upload, validate.createPost, controller.createPost)
 
 module.exports = router
