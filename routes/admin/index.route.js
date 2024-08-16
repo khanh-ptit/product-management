e9ccadce84988ed8f207436ff66ba099f3006dac
khanh-ptit <168727980+khanh-ptit@users.noreply.github.com>
2024-08-16 22:52:12 +0700

    Xây dựng trang thông tin tài khoản + chỉnh sửa bên admin

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -4,6 +4,7 @@ const productCategoryRoutes = require("./product-category.route")
 const rolesRoutes = require("./role.route")
 const accountRoutes = require("./account.route")
 const authRoutes = require("./auth.route")
+const myAccountRoutes = require("./my-account.route")
 
 const authMiddleware = require("../../middlewares/admin/auth.middleware")
 
@@ -18,4 +19,5 @@ module.exports = (app) => {
     app.use(PATH_ADMIN + '/roles', authMiddleware.requireAuth, rolesRoutes)
     app.use(PATH_ADMIN + '/accounts', authMiddleware.requireAuth, accountRoutes)
     app.use(PATH_ADMIN + '/auth', authRoutes)
+    app.use(PATH_ADMIN + '/my-account', authMiddleware.requireAuth, myAccountRoutes)
 }
