f88f1c9ec8b54ba237ea948c68b1ffb28ccece68
khanh-ptit <168727980+khanh-ptit@users.noreply.github.com>
2024-08-18 23:59:03 +0700

    Hiển thị phần sản phẩm nổi bật

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,6 +1,18 @@
+const Product = require("../../models/product.model")
+const productHelper = require("../../helpers/product")
+
 // [GET] /
 module.exports.index = async (req, res) => {
+    const featuredProducts = await Product.find({
+        featured: "1",
+        deleted: false,
+        status: "active"
+    })
+
+    const newProducts = productHelper.getNewPrice(featuredProducts)
+
     res.render("client/pages/home/index.pug", {
         pageTitle: "Trang chủ",
+        featuredProducts: newProducts
     })
 }
--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,17 +1,17 @@
 const Product = require("../../models/product.model")
-
+const productHelper = require("../../helpers/product")
 
 // [GET] /products
 module.exports.index = async (req, res) => {
     const products = await Product.find({
         status: "active",
         deleted: false
+    }).sort({
+        position: "desc"
     })
     // console.log(products)
-    const newProducts = products.map(item => {
-        item.priceNew = (item.price * (100 - item.discountPercentage) / 100).toFixed(0)
-        return item
-    })
+    const newProducts = productHelper.getNewPrice(products)
+    
     res.render("client/pages/products/index.pug", {
         pageTitle: "Trang danh sách sản phẩm",
         products: newProducts
