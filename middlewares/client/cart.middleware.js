9950cbbf3953dc8bfdd3eea24a34a1595a32b964
khanh-ptit <168727980+khanh-ptit@users.noreply.github.com>
2024-08-24 23:55:03 +0700

    fix bug tạo nhiều giỏ hàng (kh biết có oke không =)

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -1,31 +1,35 @@
 const Cart = require("../../models/cart.model")
 
 module.exports.cartId = async (req, res, next) => {
-    // console.log(req.cookies.cartId)
-    if (!req.cookies.cartId) {
-        const cart = new Cart()
-        await cart.save()
-
-        const expiresTime = 1000 * 86400
-
-        res.cookie("cartId", cart.id, {
-            expires: new Date(Date.now() + expiresTime)
-        })
-        // console.log(cart)
-        // res.co
-    } else {
+    try {
         // console.log(req.cookies.cartId)
-        const cart = await Cart.findOne({
-            _id: req.cookies.cartId
-        })
-        let totalQuantity = 0
-        cart.products.forEach(product => {
-            totalQuantity += product.quantity
-        })
-        // console.log(totalQuantity)
-        cart.totalQuantity = totalQuantity
-        res.locals.miniCart = cart
-        // console.log(cart)
+        if (!req.cookies.cartId) {
+            const cart = new Cart()
+            await cart.save()
+
+            const expiresTime = 1000 * 86400
+
+            res.cookie("cartId", cart.id, {
+                expires: new Date(Date.now() + expiresTime)
+            })
+            // console.log(cart)
+            // res.co
+        } else {
+            // console.log(req.cookies.cartId)
+            const cart = await Cart.findOne({
+                _id: req.cookies.cartId
+            })
+            let totalQuantity = 0
+            cart.products.forEach(product => {
+                totalQuantity += product.quantity
+            })
+            // console.log(totalQuantity)
+            cart.totalQuantity = totalQuantity
+            res.locals.miniCart = cart
+            // console.log(cart)
+        }
+        next()
+    } catch {
+
     }
-    next()
 }
