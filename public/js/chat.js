e891c9db5959671071c8620ee032eb85b8856c9d
khanh-ptit <168727980+khanh-ptit@users.noreply.github.com>
2024-08-30 19:27:19 +0700

    Làm tính năng chat (part 1)

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,9 @@
-var socket = io()
+// CLIENT_SEND_MESSAGE
+const formSendData = document.querySelector(".chat .inner-form")
+if (formSendData) {
+    formSendData.addEventListener("submit", (e) => {
+        e.preventDefault()
+        console.log(e.target.elements.content.value)
+    })
+}
+// End CLIENT_SEND_MESSAGE
