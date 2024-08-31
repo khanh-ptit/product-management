4ba78e12c687c9bc0aa0cab04629b6c95b82a42a
khanh-ptit <168727980+khanh-ptit@users.noreply.github.com>
2024-08-31 11:36:55 +0700

    Làm tính năng chat (part 2)

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,9 +1,23 @@
+const Chat = require("../../models/chat.model")
+
 // [GET] /chat
 module.exports.index = (req, res) => {
-    _io.on('connection', (socket) => {
-        console.log('a user connected', socket.id)
+    const userId = res.locals.user.id
+    // SOCKET io
+    _io.once('connection', (socket) => {
+        socket.on("CLIENT_SEND_MESSAGE", async (content) => {
+            // console.log(content, userId)
+            const record = new Chat({
+                user_id: userId,
+                content: content
+            })
+            await record.save()
+        })
+        // console.log('a user connected', socket.id)
     })
+    // End SOCKET io
     
+    console.log("*")
     res.render("client/pages/chat/index.pug", {
         pageTitle: "Chat"
     })
--- public/js/chat.js
+++ public/js/chat.js
@@ -3,7 +3,11 @@ const formSendData = document.querySelector(".chat .inner-form")
 if (formSendData) {
     formSendData.addEventListener("submit", (e) => {
         e.preventDefault()
-        console.log(e.target.elements.content.value)
+        const content = (e.target.elements.content.value)
+        if (content) {
+            socket.emit("CLIENT_SEND_MESSAGE", content)
+            e.target.elements.content.value = ""
+        }
     })
 }
 // End CLIENT_SEND_MESSAGE
